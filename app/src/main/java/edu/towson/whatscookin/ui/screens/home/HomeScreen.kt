99658a9c1b4a40b82d6c07b4a5d243d79fe10164
Ezekiel <ehalle4@students.towson.edu>
2022-11-19 21:06:21 -0500

    Set up basic home screen

--- app/src/main/java/edu/towson/whatscookin/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/edu/towson/whatscookin/ui/screens/home/HomeScreen.kt
@@ -1,8 +1,67 @@
 package edu.towson.whatscookin.ui.screens.home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(){
+    Column() {
+        Row(horizontalArrangement = Arrangement.Center) {
+            Text(
+                text = "What's Cookin'?",
+                fontSize = 45.sp,
+                modifier = Modifier.padding(30.dp)
+            )
+        }
+        Row() {
+            Menu()
+        }
+    }
 
+}
+
+@Composable
+private fun Menu(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+
+        ) {
+        MenuRow("Pantry")
+        MenuRow("Recipes")
+        MenuRow("Tools")
+    }
+}
+
+@Composable
+private fun MenuRow(text: String){
+    Row(){
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = text,
+                fontSize = 35.sp,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview(){
+    HomeScreen()
 }
