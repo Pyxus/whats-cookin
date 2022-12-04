8776101cbf260486827f55af933177ca42087c8a
Katie_Durkin <kdurki3@students.towson.edu>
2022-12-04 18:42:15 -0500

    My Pantry Items

--- app/src/main/java/edu/towson/whatscookin/ui/screens/addpantryitem/AddPantryItemScreen.kt
+++ app/src/main/java/edu/towson/whatscookin/ui/screens/addpantryitem/AddPantryItemScreen.kt
@@ -1,7 +1,10 @@
 package edu.towson.whatscookin.ui.screens.addpantryitem
 
+import android.graphics.Color.parseColor
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -9,13 +12,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import edu.towson.whatscookin.ui.screens.home.HomeScreen
-//import edu.towson.whatscookin.ui.screens.home.MenuRow
 
-//import edu.towson.whatscookin.ui.screens.home.MenuRow
 
 
 
@@ -31,8 +33,6 @@ fun AddPantryItemScreen(){
                 fontSize =45.sp
             )
         }
-
-        FoodItem()
 
     }
     AddNewItem()
@@ -53,7 +53,10 @@ Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.Cente
 verticalArrangement = Arrangement.Center
 ) {
 
-    TextField (value = "Ex: Lettuce", onValueChange = {})
+    OutlinedTextField (value = "ITEM", onValueChange = {})
+    OutlinedTextField (value = "DATE", onValueChange = {})
+    OutlinedTextField (value = "QUANTITY", onValueChange = {})
+    OutlinedTextField (value = "EXPIRATION", onValueChange = {})
 
 
 //    var text by remember {
@@ -88,6 +91,7 @@ private fun AddNewItem(){
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxHeight()
+            //.background(color = "#2596be".color)
 
     ) {
         NewRow("Add New Item")
@@ -105,6 +109,7 @@ private fun NewRow(text: String){
             modifier = Modifier
                 .padding(20.dp)
                 .fillMaxWidth()
+                .background(color = "#2596be".color)
         ) {
             Text(
                 text = text,
@@ -119,7 +124,8 @@ private fun NewRow(text: String){
 
 
 
-
+val String.color
+get()= Color(parseColor(this))
 
 
 @Composable
--- app/src/main/java/edu/towson/whatscookin/ui/screens/pantry/PantryScreen.kt
+++ app/src/main/java/edu/towson/whatscookin/ui/screens/pantry/PantryScreen.kt
@@ -3,12 +3,14 @@ package edu.towson.whatscookin.ui.screens.pantry
 import android.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,48 +19,21 @@ import edu.towson.whatscookin.ui.screens.home.HomeScreen
 
 
 
+
 @Composable
-fun PantryScreen(){
+fun PantryScreen(
+//    food: List<Food>,
+//    modifier: Modifier = Modifier
 
-    Column(
-        horizontalAlignment = Alignment.Companion.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                   // .background(color.Blue)
+){
 
-    ){
-
-        Row (){
-            Text("Pantry", fontSize =45.sp)
-        }
-
-
-
-
-
-
-        Row(
-            modifier = Modifier.fillMaxSize()
-        ){
-            Button(onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .height(60.dp)
-                    .width(200.dp)
-
-                ) {
-                
-                Text(text = "Add", fontSize = 29.sp)
-                
-            }
-            
-        }
-    }
-    
-    
-    
-    
-    
+//    LazyColumn(
+//        state = listState,
+//
+//    )
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
