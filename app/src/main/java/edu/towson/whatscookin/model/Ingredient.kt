0a0736e9cda8418652e9bac5e6f51ce7cd7a9d53
John Appel <jappel2@students.towson.edu>
2022-12-09 01:29:45 -0500

    Made only the StoredIngredient data class an entity

--- app/src/main/java/edu/towson/whatscookin/model/Ingredient.kt
+++ app/src/main/java/edu/towson/whatscookin/model/Ingredient.kt
@@ -4,16 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// sqlite ingredient table
-@Entity(tableName = "ingredients")
 data class Ingredient(
-    @PrimaryKey
+
     val id: Int,
-    @ColumnInfo(name = "ingredient_name")
     val name: String,
-    @ColumnInfo(name = "ingredient_description")
     val description: String?,
-    @ColumnInfo(name = "ingredient_image_url")
     val imageUrl: String?
 
     ){
